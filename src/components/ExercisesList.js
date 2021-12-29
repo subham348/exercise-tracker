1584963ea4ad98a2f3c4f7bd196f73622802689d
subham348 <subhammahato348@gmail.com>
2021-12-30 00:23:00 +0530

    first commit

---
+++ src/components/ExercisesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the exercises list component</p>
+      </div>
+    );
+  }
+}
+
+export default ExercisesList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">
+          ExcerTrack
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            {/* <li className="navbar-item"> */}
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Exercises
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/create" className="nav-link">
+                Create Exercise Log
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/user" className="nav-link">
+                Create User
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
